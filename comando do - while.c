124b2bef73def82c88cabb9a4e1832345d8d26fe
Davi Paulino Conceição <163527003+Davi2910@users.noreply.github.com>
2024-09-01 15:32:34 -0300

    Add files via upload

---
+++ comando do - while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){ // do while.
+
+    int  i=1;
+
+    do{
+        printf("%d ", i);
+        i++; 
+    }while(i <= 10);
+
+}
---
+++ comando while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){ // enquanto for verdadeiro o codigo e executavel.
+
+
+    int i=1;
+
+    while(i <= 10){
+        printf("%d ", i);
+        i++;
+    }
+
+}
